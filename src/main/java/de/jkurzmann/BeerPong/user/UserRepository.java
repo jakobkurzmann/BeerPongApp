b589393d684b81f5ef0470109daa84948ecb1bf3
qQuatschMitSosse <business.kurzmann@gmail.com>
2024-05-21 23:28:14 +0200

    Implement CRUD (Create, Read, Update, Delete) operations for users in the database.

--- src/main/java/de/jkurzmann/BeerPong/user/UserRepository.java
+++ src/main/java/de/jkurzmann/BeerPong/user/UserRepository.java
@@ -1,53 +1,63 @@
 package de.jkurzmann.BeerPong.user;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class UserRepository
-{
-    private List<User> users = new ArrayList<>();
+public class UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(UserRepository.class);
+    private final JdbcClient jdbcClient;
 
-    List<User> findAll()
+    public UserRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+    //This function retrieves all users from the database
+    public List<User> findAll() {
+        return jdbcClient.sql("select * from \"User\"")
+                .query(User.class)
+                .list();
+    }
+    //This function searches for a user in the database based on their ID.
+    public Optional<User> findById(Integer id) {
+        return jdbcClient.sql("select * from \"User\" WHERE id = :id")
+                .param("id", id)
+                .query(User.class)
+                .optional();
+    }
+    //This function creates a new user and adds them to the database.
+    public void createUser(User user)
     {
-        return users;
+        var updated = jdbcClient.sql("INSERT INTO \"User\"(id, username, firstname, " +
+                "lastname, email, profilePicture) values(?,?,?,?,?,?)").params(List.of(user.id(), user.username(),
+                user.firstname(), user.lastname(), user.email(), user.profilePicture())).update();
+
+        Assert.state(updated == 1, "Failed to create user" + user.username());
+    }
+    //This function updates an existing user in the database based on their ID.
+    public void updateUser(User user, Integer id)
+    {
+        var updated = jdbcClient.sql("UPDATE \"User\" set id = ?, username = ?, firstname = ?," +
+                                            "lastname = ?, email = ?, profilePicture = ? WHERE id = ?")
+                .params(List.of(user.id(), user.username(),
+                        user.firstname(), user.lastname(), user.email(), user.profilePicture(),id)).update();
+
+        Assert.state(updated == 1, "Failed to update user " + user.id());
+    }
+    //This function deletes a user from the database based on their ID.
+    public void deleteUser(Integer id)
+    {
+        var updated = jdbcClient.sql("DELETE FROM \"User\" WHERE id = :id").param("id",id).update();
+
+        Assert.state(updated == 1, "Failed to delete User " + id) ;
     }
 
-    Optional<User> findById(Integer id)
-    {
-        return users.stream()
-                .filter(users -> users.id() == id)
-                .findFirst();
-    }
 
-    void createUser(User user)
-    {
-        users.add(user);
-    }
-
-    void updateUser(User user, Integer id)
-    {
-        Optional<User> existingUser = findById(id);
-        if(existingUser.isPresent())
-        {
-            users.set(users.indexOf(existingUser.get()),user);
-        }
-    }
-
-    void deleteUser(Integer id)
-    {
-        users.removeIf(user -> user.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init()
-    {
-        users.add(new User(1,"JJK","business.kurzmann@gmail.com", "Jakob", "Kurzmann",
-                ""));
-        users.add(new User(2,"Incasula", "incasula@gmx.com", "Susanne", "Lalla",""));
-    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "User" (id, username, firstname, lastname, email, profilePicture)
+VALUES (1, 'JJK', 'Jakob','Kurzmann', 'business.kurzmannn@gmail.com','');
