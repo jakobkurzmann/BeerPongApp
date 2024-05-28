e72494d35e7094b574bf39407feafb1deff46895
qQuatschMitSosse <business.kurzmann@gmail.com>
2024-05-28 15:19:52 +0200

    Added a postgres database to the project and removed the data file

--- src/main/java/de/jkurzmann/BeerPong/user/UserRepository.java
+++ src/main/java/de/jkurzmann/BeerPong/user/UserRepository.java
@@ -3,6 +3,7 @@ package de.jkurzmann.BeerPong.user;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "User" (id, username, firstname, lastname, email, profilePicture)
-VALUES (1, 'JJK', 'Jakob','Kurzmann', 'business.kurzmannn@gmail.com','');
